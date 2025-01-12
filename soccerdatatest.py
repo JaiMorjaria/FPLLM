e1dcb1bb9eebf4894edb930698e75bdea62bea2b
Jai Morjaria <jaimorjaria2099@gmail.com>
2025-01-11 19:33:32 -0500

    Created name mapping between players on FPL API endpoint and Understat

--- soccerdatatest.py
+++ soccerdatatest.py
@@ -1,14 +1,56 @@
 import soccerdata as sd
 import pandas as pd
+import json
 from classes import Player, Game, Event
 import datetime
+import requests
+from fuzzywuzzy import process
+from unidecode import unidecode
 
 
+bootstrap_static_url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+bootstrap_static_raw = requests.get(url=bootstrap_static_url).json()
+player_data_fpl_site = bootstrap_static_raw["elements"]
+player_web_names = [player["web_name"] for player in player_data_fpl_site]
 understat = sd.Understat(leagues="ENG-Premier League", seasons="2024/2025")
+player_data_understat = understat.read_player_season_stats().reset_index()
+understat_names = player_data_understat["player"]
 
-data = understat.read_team_match_stats()
 
-# use https://fantasy.premierleague.com/api/bootstrap-static/ to get injury reports
+
+def fuzzy_match(player_name):
+    match, score, index = process.extractOne(player_name, understat_names)
+    return match if score >= 75 else score  # Adjust threshold
+
+
+
+names_mapping = {
+    "Son": "Son Heung-Min",
+    "Wood": "Chris Wood",
+}
+for player in player_data_fpl_site:
+    # if player's news doesn't include the word "injury" they're probably on loan or transferred out of the league
+    #  and therefore don't need to be fuzzy matched
+    # if their TSB% < 1.0, most people aren't even going to think about them as a pick
+    # if they have < 10 points by this point in the season they probably don't play
+    if (player["news"] != "" and "injury" not in player["news"].lower()) or float(player["selected_by_percent"]) < 1.0 or player["total_points"] < 10:
+        continue
+    else:
+        if player["web_name"] not in names_mapping:
+            match = fuzzy_match(player["web_name"])
+            if type(match) is str:
+                names_mapping[player["web_name"]] =  match
+                continue
+            else:
+                #if we don't have a strong match with the web_name, try again with first and last name
+                names_mapping[player["web_name"]] = fuzzy_match(player["first_name"] + " " + player["second_name"])
+
+with open('names_mapping.json', 'w', encoding='utf-8') as f:
+    json.dump(names_mapping, f, ensure_ascii=False, indent=4)
+
+
+
+# use htxtps://fantasy.premierleague.com/api/bootstrap-static/ to get injury reports
 # use web_name instead of relying on gpt to translate the name 
 # categorize team  average xG, xA, goals allowed (note on xGA below)
 # categorize player average xG, xA (maybe combine into xGI), minutes per game, number of starts, injury status
--- xgtest.py
+++
@@ -1,8 +0,0 @@
-import soccerdata as sd
-
-fbref = sd.FBref(leagues="ENG-Premier League", seasons=2425)
-team_match_stats = fbref.read_team_match_stats(stat_type="schedule", team="Manchester City")
-player_match_stats = fbref.read_player_match_stats(stat_type="summary", match_id='67a0c715')
-player_match_stats = player_match_stats.reset_index()
-print(player_match_stats.index)
-print('Jerry Mice')
